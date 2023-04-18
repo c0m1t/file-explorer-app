9f5a79cf281b83953ec31bfcaaf4692259123bdc
Shahab Saeeda <>
2023-04-18 15:35:46 +0100

    chore(deps): install @ianvs/prettier-plugin-sort-imports 3.7.2

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,16 @@
-/** @type {import('prettier').Config} */
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
   endOfLine: "lf",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+  ],
+  importOrderCombineTypeAndValueImports: true,
+  importOrderMergeDuplicateImports: true,
+  importOrderSortSpecifiers: true,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
   singleQuote: false,
   tabWidth: 2,
   trailingComma: "es5",
-}
+};
